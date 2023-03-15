b1091c33ca06968debb6b7cea299287835ec8e77
Robert <r.s.garofalo87@gmail.com>
2023-03-16 08:50:33 +1100

    add header right bell and profile icons

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 // RN vector icons
 import FAIcon from 'react-native-vector-icons/FontAwesome'
 import EntypoIcon from 'react-native-vector-icons/Entypo'
+import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+import Ionicon from 'react-native-vector-icons/Ionicons'
 
 // SCREENS
 import Feed from './screens/main/Feed';
@@ -40,32 +42,51 @@ function getHeaderTitle(route){
 
 // Tab Navigator
 const Tab = createBottomTabNavigator()
+
+// tab bar active / inactive colors
+const activeColor = '#1e6eca' 
+const inactiveColor = '#a2adbb'
+
 function HomeScreen({ navigation, route }){
+  const routeName = getHeaderTitle(route)
+
   return (
     <Tab.Navigator screenOptions={{
-      headerShown: false
+      headerShown: false,
+      tabBarActiveTintColor: activeColor,
+      tabBarInactiveTintColor: inactiveColor
     }} >
       <Tab.Screen name="Feed" component={Feed} options={{
-      tabBarIcon: () =>  <FAIcon name='feed' size={20} />
+      tabBarIcon: ({ focused }) =>  
+      <MCIcon 
+      name='view-dashboard' 
+      size={20} 
+      color={focused ? activeColor : inactiveColor} 
+      />
       }}/>
       <Tab.Screen name="Progress" component={Progress} 
       options={{
-      tabBarIcon: () =>  <FAIcon name='bar-chart' size={20} />
+      tabBarIcon: ({ focused }) =>  <EntypoIcon name='bar-graph' size={20} color={focused ? activeColor : inactiveColor}/>
       }}
       />
       <Tab.Screen name="Null" component={EmptyScreen} 
       options={{
         tabBarLabel:() => { return null}, 
-        tabBarIcon: () =>  <Image 
-        style={{ width: 60, height: 60, backgroundColor: 'red', borderRadius: 50,}}
-        source={require('./assets/favicon.png')}
+        tabBarIcon: () =>  
+        <>
+        <View style={styles.tabBar.mainButton}>
+          <Image 
+        style={styles.tabBar.mainButtonImage}
+        source={require('./assets/v2.png')}
         />
+        </View>
+        </>
       }}/>
       <Tab.Screen name="Store" component={Store} options={{
-      tabBarIcon: () =>  <FAIcon name='shopping-bag' size={20} />
+      tabBarIcon: ({ focused }) =>  <FAIcon name='shopping-bag' size={20} color={focused ? activeColor : inactiveColor}/>
       }}/>
       <Tab.Screen name="Menu" component={Menu} options={{
-      tabBarIcon: () =>  <EntypoIcon name='dots-three-horizontal' size={20} />
+      tabBarIcon: ({ focused }) =>  <EntypoIcon name='dots-three-horizontal' size={20} color={focused ? activeColor : inactiveColor}/>
       }}/>
 
     </Tab.Navigator>
@@ -79,18 +100,29 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'
       screenOptions={{
-          headerStyle: {
-            backgroundColor: '#f4511e'
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-          }
       }}>
         <Stack.Screen 
         name="Home" 
         component={HomeScreen} 
         options={({route, navigation}) => ({ 
-          headerTitle: getHeaderTitle(route),        
+          headerShadowVisible: false,
+          headerTitle: '',
+          headerRight: () => 
+          <View style={styles.headerRight}>
+              <View>
+                <MCIcon 
+                name='bell' 
+                size={20} 
+                color={inactiveColor}
+                />
+                <View style={styles.bellBadge} />
+            </View>
+            <Ionicon 
+              name='person' 
+              size={20} 
+              color={inactiveColor} 
+              />
+            </View>
           })}/>
         {/* <Stack.Screen name="Details" component={DetailsScreen} options={({ route }) => ({title: route.params.productName})} initialParams={{initialId: 99}}/> */}
         {/* <Stack.Screen name="Create Post" component={CreatePost} /> */}
@@ -106,6 +138,42 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerRight: {
+    flexDirection: 'row',
+    width: 65, 
+    height: 25,
+    marginRight: 5,
+    marginBottom: 10,
+    justifyContent: 'space-between'
+  },
+  bellBadge: {
+    width: 10, 
+    height: 10,
+    borderRadius: 50,
+    borderColor: '#fff',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    backgroundColor: 'red', 
+    position: 'absolute', 
+    top: 0, 
+    right: 0,
+  },
+  tabBar: {
+    mainButton: {
+      backgroundColor: '#e44384', 
+      width: 45, 
+      height: 45,
+      marginTop: 5, 
+      borderRadius: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingTop: 5,
+    },
+    mainButtonImage: {
+      width: 18, 
+      height: 18
+    }
+  }
 });
 
 
